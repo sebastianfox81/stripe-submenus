9d81e5a2b9ecc2424630d6a13a989c05632038bd
Sebastian <sebastianfox81@gmail.com>
2022-01-28 16:22:15 -0500

    Implemented useEffect for change in links array size

--- client/src/Submenu.js
+++ client/src/Submenu.js
@@ -4,19 +4,40 @@ import { useGlobalContext } from './context'
 
 const Submenu = () => {
 
-  const { isSubmenuOpen, location } = useGlobalContext();
+  const { isSubmenuOpen, location, page : {page, links} } = useGlobalContext();
   const container = useRef(null);
+  const [ columns, setColumns ] = useState('col-2')
 
   useEffect(() => {
+    setColumns('col-2')
     const submenu = container.current;
     const { center, bottom } = location;
     submenu.style.left = `${center}px`;
     submenu.style.top = `${bottom}px`;
-  },[location])
+
+    if (links.length === 3) {
+      setColumns('col-2')
+    }
+    if (links.length > 3) {
+      setColumns('col-4')
+    }
+
+  },[location, links])
 
   return (
     <aside className={isSubmenuOpen ? 'submenu show' : 'submenu'} ref={container}>
-      Submenu
+      <h4>{page}</h4>
+      <div className={`submenu-center ${columns}`}>
+        {links.map((link) => {
+          const { label, icon, url } = link;
+          return (
+            <a href={url}>
+              {icon}
+              {label}
+            </a>
+          )
+        })}
+      </div>
     </aside>
   )
 }
