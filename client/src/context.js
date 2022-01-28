feb06ae1adbf0ff0913a9a3522dd8447a4c4d959
Sebastian <sebastianfox81@gmail.com>
2022-01-28 15:54:01 -0500

    Implemented submenu onMouse behaviour

--- client/src/context.js
+++ client/src/context.js
@@ -1,34 +1,47 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import React, { useState, useContext } from "react";
+import sublinks from "./data";
 
 const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-
-  const [ isSidebarOpen, setIsSidebarOpen ] = useState(false);
-  const [ isSubmenuOpen, setIsSubmenuOpen ] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+  const [location, setLocation] = useState({});
 
   const openSidebar = () => {
-    setIsSidebarOpen(true)
-  }
+    setIsSidebarOpen(true);
+  };
 
   const closeSidebar = () => {
-    setIsSidebarOpen(false)
-  }
+    setIsSidebarOpen(false);
+  };
 
-  const openSubmenu = () => {
-    setIsSubmenuOpen(true)
-  }
+  const openSubmenu = (text, coordinates) => {
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
 
   const closeSubmenu = () => {
-    setIsSubmenuOpen(false)
-  }
+    setIsSubmenuOpen(false);
+  };
 
-  return <AppContext.Provider value={{
-    isSidebarOpen, isSubmenuOpen, openSidebar, closeSidebar, openSubmenu, closeSubmenu
-  }}>{children}</AppContext.Provider>
-}
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+        location,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
